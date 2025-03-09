457ed947c32e28b1e7e7bf6c9c976cdf9c265f14
Jeremy Kepf <jeremy1975@gmail.com>
2025-03-08 22:25:26 -0500

    dockerfile

--- obj/Debug/net9.0/back-end.AssemblyInfo.cs
+++ obj/Debug/net9.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13628336911e69694584be142d741b1369bf6e2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa36c959c365c3a426d5c3701b74f37923a7e97")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
