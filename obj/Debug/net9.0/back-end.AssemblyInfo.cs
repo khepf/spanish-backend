4f02b860bf1a0ee568b244965c5696e70ca4976f
Jeremy Kepf <jeremy1975@gmail.com>
2025-03-11 20:31:20 -0400

    hosting issues3

--- obj/Debug/net9.0/back-end.AssemblyInfo.cs
+++ obj/Debug/net9.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b02d438cd85e87ddb72b5405ec687ddfc9300bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf58138865286585760ecf711aa7e77d982db05")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
